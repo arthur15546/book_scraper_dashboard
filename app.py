d973fae244f9e941ae67add946e8d9a62707f617
Athur Maciel <arthurmaciel1546@gmail.com>
2026-02-25 21:56:32 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import extrair_dados
+
+
+st.set_page_config(layout="wide", page_title="scraper de livros", page_icon='📚')
+st.title("📚 Scraper de livros")
+st.write("Clique no botão para coletar dados do site books to scrape.")
+
+st.divider()
+
+st.header("Coleta de dados")
+
+
+
+if st.button("Coletar 🚀"):
+    with st.spinner("Coletando dados..."):
+        extrair_dados.coleta()
+
+    st.success("Coleta finalizada ✅")
+
+    col1, col2, col3 = st.columns(3)
+
+    df = pd.read_csv('projeto_automação_livros.csv')
+    with col1:
+        st.metric('total de livros', len(df))
+    with col2:
+        media_valor = df['preço'].mean()
+        st.metric('media de preços', f'£{media_valor:.2f}')
+    with col3:
+        media_estrelas = df['avaliação'].mean()
+        st.metric('media de avaliação', f'{media_estrelas:.2f}')
+
+    st.subheader('📊 preço medio por avaliação')
+    grafico = df.groupby('avaliação')['preço'].mean().reset_index()
+    st.bar_chart(grafico, x='avaliação', y='preço', width=800, height=400)
+
+    st.dataframe(df, use_container_width=True)
---
+++ extrair_dados.py
@@ -0,0 +1,47 @@
+from playwright.sync_api import sync_playwright
+import csv
+
+def coleta():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto("https://books.toscrape.com")
+
+        lista_de_liros = list()
+        while True:
+            livros = page.locator("article.product_pod")
+            for l in range(livros.count()):
+                livro = livros.nth(l)
+
+                nome = livro.locator('h3 a').get_attribute('title')
+                preco = livro.locator('.price_color').inner_text()
+                preco_arrumado = preco.replace('£', '')
+                preco = float(preco_arrumado)
+                aval = livro.locator('.star-rating').get_attribute('class')
+                parte = aval.split()
+                ava = parte[1]
+                mapa = {
+                    'One':1,
+                    'Two':2,
+                    'Three':3,
+                    'Four':4,
+                    'Five':5,
+
+                }
+                avaliacao = mapa[ava]
+
+                categorias = {'nome':nome , 'preço':preco_arrumado , 'avaliação':avaliacao}
+                lista_de_liros.append(categorias)
+
+            if page.locator('li.next a').count() == 1:
+                page.click('li.next a')
+                page.wait_for_load_state('load')
+
+            else:
+                break
+
+    arquivo = open('projeto_automação_livros.csv', 'w')
+    writer = csv.DictWriter(arquivo, fieldnames=['nome', 'preço', 'avaliação'])
+    writer.writeheader()
+    for livro in lista_de_liros:
+        writer.writerow(livro)
